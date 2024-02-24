fad54a8f1fefa38e42dcc2a3b83920cd08d06da5
kseni3sold <149370788+kseni3sold@users.noreply.github.com>
2024-02-24 19:58:56 +0300

    Delete 2/soldatkins_kk directory

--- 2/soldatkins_kk/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-import pickle
-from common import ADDR
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(ADDR)
-data = pickle.loads(s.recv(500))
-for i in data:
-    print(f"got {i}")
