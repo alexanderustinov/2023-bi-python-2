e06266876918fa7b121432f4e342b36783172940
Dmitriy Vinogradov <149401057+Bibabibka@users.noreply.github.com>
2024-03-04 19:33:01 +0300

    Update server.py

--- 2/vinogradov_dm/server.py
+++ 2/vinogradov_dm/server.py
@@ -7,9 +7,9 @@ from dz import *
 logger = logging.getLogger(__name__)
 logging.basicConfig()
 
-s = socket.socket(socket.AFINET, socket.SOCKSTREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind(ADDR)
+c = s.bind(ADDR)
 s.listen(5)
 
 s.setblocking(False)
@@ -18,13 +18,13 @@ s.settimeout(1)
 while True:
     try:
         clientsocket, addr = s.accept()
-    except socket.timeout:
+    except TimeoutError:
         continue
     client, (addr, port) = clientsocket
     logger.warning(f"{addr} connected from port {port}")
 
     data = {Fr, Ci, B}
 
-    client.send(pickle.dumps(data))
+    client.send(pickle.dumps(data, protocol=pickle.HIGHEST_PROTOCOL))
 
     client.close()
