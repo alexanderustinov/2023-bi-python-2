3317c4f2bc02e3103c3ab71346091d8cb2b65706
Artem Mozalev <mozalev.art@gmail.com>
2024-03-11 22:20:28 +0300

    Асиинк Клиент-сервер

---
+++ 3/mozalev_as/cl.py
@@ -0,0 +1,36 @@
+import asyncio
+
+class EchoClientProtocol(asyncio.Protocol):
+    def __init__(self, message, on_con_lost):
+        self.message = message
+        self.on_con_lost = on_con_lost
+
+    def connection_made(self, transport):
+        transport.write(self.message.encode())
+        print(f'Клиент сказал: {self.message}')
+
+    def data_received(self, data):
+        print(f'Сервер ответил: {data.decode()}')
+
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        self.on_con_lost.set_result(True)
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    on_con_lost = loop.create_future()
+    message = 'Покажи шо умеешь'
+
+    transport, protocol = await loop.create_connection(
+        lambda: EchoClientProtocol(message, on_con_lost),
+        '127.0.0.1', 3333)
+
+    try:
+        await on_con_lost
+    finally:
+        transport.close()
+
+
+asyncio.run(main())
---
+++ 3/mozalev_as/serv.py
@@ -0,0 +1,46 @@
+import asyncio
+from classes_5 import Customer, Product
+import random
+
+def try_use_classes():
+    product1 = Product("Футболка", 500)
+    product2 = Product("Джинсы", 1000)
+            
+    quantity1 = random.randint(1, 5)
+    quantity2 = random.randint(1, 5)
+
+    customer = Customer("Иван")
+    customer.add_to_cart(product1, quantity1)
+    customer.add_to_cart(product2, quantity2)
+    return customer.view_cart()
+
+class EchoServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+
+    def data_received(self, data):
+        message = data.decode()
+        print('Клиент сказал: {}'.format(message))
+        
+        answer = try_use_classes()
+        print('Сервер ответил: {}'.format(answer))
+        self.transport.write(answer.encode())
+
+        print('Close the client socket')
+        self.transport.close()
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    server = await loop.create_server(
+        lambda: EchoServerProtocol(),
+        '127.0.0.1', 3333)
+
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main())
