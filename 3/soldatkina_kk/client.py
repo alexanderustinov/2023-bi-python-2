58db0ed48a4a0b58d1fbeccf6b630f4a7b9a4838
kseni3sold <149370788+kseni3sold@users.noreply.github.com>
2024-03-12 17:34:32 +0300

    Add files via upload

---
+++ 3/soldatkina_kk/client.py
@@ -0,0 +1,37 @@
+import pickle
+import asyncio
+from common import ADDR
+
+class EchoClientProtocol(asyncio.Protocol):
+    def __init__(self, message, on_con_lost):
+        self.message = message
+        self.on_con_lost = on_con_lost
+
+    def connection_made(self, transport):
+        transport.write(pickle.dumps(self.message))
+        print('Data sent: {!r}'.format(self.message))
+
+    def data_received(self, data):
+        print('Data received: {!r}'.format(data))
+
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        self.on_con_lost.set_result(True)
+
+async def main():
+
+    loop = asyncio.get_running_loop()
+
+    on_con_lost = loop.create_future()
+    message = 'Welcome!'
+
+    transport, protocol = await loop.create_connection(
+        lambda: EchoClientProtocol(message, on_con_lost),
+        ADDR[0], ADDR[1])
+
+    try:
+        await on_con_lost
+    finally:
+        transport.close()
+
+asyncio.run(main())
---
+++ 3/soldatkina_kk/server.py
@@ -0,0 +1,38 @@
+import logging
+from common import ADDR
+
+logger = logging.getLogger(__name__)
+logging.basicConfig()
+
+import asyncio
+
+
+class EchoServerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+
+    def data_received(self, data):
+        message = data.decode()
+        print('Data received: {!r}'.format(message))
+
+        print('Send: {!r}'.format(message))
+        self.transport.write(data)
+
+        print('Close the client socket')
+        self.transport.close()
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    server = await loop.create_server(
+        lambda: EchoServerProtocol(),
+        ADDR[0], ADDR[1])
+
+    async with server:
+        await server.serve_forever()
+
+
+asyncio.run(main())
