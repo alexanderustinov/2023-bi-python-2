f27d0f33e9b0c097244b4b1fc35314e8c5670db0
Dmitriy Vinogradov <149401057+Bibabibka@users.noreply.github.com>
2024-03-12 22:28:46 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,36 @@
+from common import AD,DR
+from dz import *
+import asyncio
+import pickle
+class EchoClientProtocol(asyncio.Protocol):
+    def __init__(self, message, on_con_lost):
+        self.message = message
+        self.on_con_lost = on_con_lost
+
+    def connection_made(self, transport):
+        transport.write(pickle.dumps(self.message))
+        print('Data sent: {!r}'.format(self.message))
+
+    def data_received(self, data):
+        print('Data received: {!r}'.format(data))
+
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        self.on_con_lost.set_result(True)
+
+async def main():
+    loop = asyncio.get_event_loop()
+    on_con_lost = loop.create_future()
+    data1 = [Fr]
+    data = pickle.dumps(data1)
+
+    transport, protocol = await loop.create_connection(
+        lambda: EchoClientProtocol(data1, on_con_lost), AD, DR)
+
+    try:
+        await on_con_lost
+    finally:
+        transport.close()
+
+if __name__ == '__main__':
+    asyncio.run(main(), debug=True)
