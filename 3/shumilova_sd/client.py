5211f3a5adee76507dba175b968e663784b8fe51
sonmur <149419693+sonmur@users.noreply.github.com>
2024-03-18 17:10:00 +0300

    Update client.py

--- 3/shumilova_sd/client.py
+++ 3/shumilova_sd/client.py
@@ -1,5 +1,9 @@
 import asyncio
 from common import ADDR
+from random import randint
+import pickle
+
+from classes import Stars, Planets
 
 class EchoClientProtocol(asyncio.Protocol):
     def __init__(self, message, on_con_lost):
@@ -7,7 +11,7 @@ class EchoClientProtocol(asyncio.Protocol):
         self.on_con_lost = on_con_lost
 
     def connection_made(self, transport):
-        transport.write(self.message.encode())
+        transport.write(pickle.dumps(self.message))
         print('Data sent: {!r}'.format(self.message))
 
     def data_received(self, data):
@@ -17,15 +21,20 @@ class EchoClientProtocol(asyncio.Protocol):
         print('The server closed the connection')
         self.on_con_lost.set_result(True)
 
+rand = randint(0, 2)
 
 async def main():
     loop = asyncio.get_running_loop()
 
     on_con_lost = loop.create_future()
-    message = 'Hello, my friend!'
+    if rand == 0:
+        datan = Stars(6.9551*(10**8), 1.9885*(10**30), 1.496*(10**11))
+    else:
+        datan = Planets(6378.1*(10**3), 465.1, 5.9726*(10**24), 12742)
+    data = pickle.dumps(datan)
 
     transport, protocol = await loop.create_connection(
-        lambda: EchoClientProtocol(message, on_con_lost),
+        lambda: EchoClientProtocol(datan, on_con_lost),
         ADDR[0], ADDR[1])
 
     try:
