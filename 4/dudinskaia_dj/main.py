2eb745b97f974f620772c3b1021581f43fb4d364
Alexander B. Ustinov <ustinov@spbau.ru>
2024-03-25 14:51:27 +0300

    enhance

--- 4/dudinskaia_dj/main.py
+++ 4/dudinskaia_dj/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect
 from sympy import prime
 
 app = Flask(__name__)
@@ -6,10 +6,15 @@ app = Flask(__name__)
 def generate_primes(n):
     return [prime(i) for i in range(1, n+1)]
 
+@app.route('/')
+def redirect_primes():
+    return redirect('/primes/42')
+
+
 @app.route('/primes/<int:n>')
 def show_primes(n):
     primes = generate_primes(n)
-    return f"The first {n} prime numbers are: {primes}"
+    return f"The first {n} prime numbers are: {primes}<br><br>(just use the address bar)"
 
 if __name__ == '__main__':
     app.run(debug=True)
