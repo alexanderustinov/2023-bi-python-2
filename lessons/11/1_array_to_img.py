b228aec504e27f713cba83b6a8bd00d824de994c
Alexander B. Ustinov <ustinov@spbau.ru>
2024-05-02 12:12:25 +0300

    11 без переполнения

--- lessons/11/1_array_to_img.py
+++ lessons/11/1_array_to_img.py
@@ -3,13 +3,14 @@ import PIL.Image
 
 size = 300
 dtype = np.uint8
-X = np.linspace(0, 255, size, dtype=dtype)
+# при X, Y от 0 до 255 Z~X**2+Y**2 довольно быстро переполняется
+X = np.linspace(0, 16, size, dtype=dtype)
 Y = X.copy()
 Z = np.zeros([3], dtype=dtype)
 X, Y, Z = np.meshgrid(X, Y, Z)
 # print(X, Y)
 
-img_data = 100*(X**2+Y**2)
+img_data = (X**2+Y**2)
 img_data[:,:,1] = np.zeros_like(img_data[:,:,1])
 img_data[:,:,2] = np.zeros_like(img_data[:,:,2])
 # slice_ = np.slice(1,2)
