1a112611dd3980377900c88036522db842a1476b
JohnThirdt <lvantretiyakov2018@gmail.com>
2024-05-20 20:47:41 +0300

    add (с кем) + штука, которая делает README.md

--- 8/tretyakov_iv/decrypt.py
+++ 8/tretyakov_iv/decrypt.py
@@ -13,20 +13,23 @@ home = Path.home()
 pk_sk_pair = pkl.PrivateKeyList.from_string(open(home / '.ssh' / 'id_rsa').read())
 own_private_key = pk_sk_pair[0].private.params.convert_to(rsa.RSAPrivateKey)
 own_public_key = pk_sk_pair[0].public.params.convert_to(rsa.RSAPublicKey)
-for smn in EVERYONE:
-    public_key = smn.get('pubkey')
-    data_encoded = ''
-    messages = smn.get('data')
-    for i in messages:
-        plaintext = own_private_key.decrypt(
-            i,
-            padding.OAEP(
-                mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                algorithm=hashes.SHA256(),
-                label=None
+with open('README.md', 'w') as f:
+    for smn in EVERYONE:
+        public_key = smn.get('pubkey')
+        data_encoded = ''
+        messages = smn.get('data')
+        for i in messages:
+            plaintext = own_private_key.decrypt(
+                i,
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
             )
-        )
-        data_encoded += plaintext.decode()
-    pers_res = jwt.decode(data_encoded, public_key, ['RS512'])
-    for i in pers_res:
-        print(pers_res[i])
+            data_encoded += plaintext.decode()
+        pers_res = jwt.decode(data_encoded, public_key, ['RS512'])
+        for i in pers_res:
+            print(pers_res[i])
+            f.write(pers_res[i]+'\n\n')
+    f.write('_PHE_IVAN_')
