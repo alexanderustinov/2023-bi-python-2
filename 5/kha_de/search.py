396bcd15248940823711a9ff96d16f5cc5d70300
Diyoni5 <90685119+Diyoni5@users.noreply.github.com>
2024-05-21 22:56:22 +0300

    Vocabulary

--- 5/kha_de/search.py
+++ 5/kha_de/search.py
@@ -6,11 +6,11 @@ with Session(engine) as s:
     search_1 = select(Book).where(Book.percent <= 30)
     search_2 = select(Book).where(Book.country == 'Morocco')
 
-    print("Countries with a value of percent of students who could not read less than 30:")
+    print("Countries where the percentage of students who could not read was less than 30:")
     for i in s.scalars(search_1):
         print(f"- {i.country} - {i.percent}%")
 
     print()
 
     for i in s.scalars(search_2):
-        print(f"In Morocco percent of students who could not read is {i.percent}.")
+        print(f"In Morocco percent of students who could not read was {i.percent}.")
