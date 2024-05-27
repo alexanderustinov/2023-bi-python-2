bf183b9757df218f3b7dcb01187ac68dfb74a167
Auchindoun <christhornd@gmail.com>
2024-05-27 18:47:17 +0300

    'parfenov_gs_5_667'

--- 5/parfenov_gs/main.py
+++ 5/parfenov_gs/main.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import sqlalchemy
 from sqlalchemy import Column, Integer, String, create_engine, select
 from sqlalchemy.orm import declarative_base, Session
@@ -21,39 +22,43 @@ class AviationAccidents(Base):
     Landing_accidents = Column(Integer)
     Landing_fatalities = Column(Integer)
 
-engine = sqlalchemy.create_engine('sqlite:///idk.db', echo=False)
-Base.metadata.create_all(engine)
+if not(os.path.exists('idk.db')):
+    engine = sqlalchemy.create_engine('sqlite:///idk.db', echo=False)
 
-with open('Aviation accidents and fatalities by flight phase (ASN, 2019).csv', encoding='utf-8', mode='r') as file:
-    data = csv.reader(file, delimiter=',')
-    data.__next__()
+    Base.metadata.create_all(engine)
 
-    with Session(engine) as sess:
-        for i in data:
-            new = AviationAccidents(
-                Entity=i[0],
-                Year=int(i[1]),
-                En_route_accidents=int(i[2]),
-                En_route_casualties=int(i[3]),
-                Take_off_accidents=int(i[4]),
-                Take_off_casualties=int(i[5]),
-                Initial_climb_accidents=int(i[6]),
-                Initial_climb_casualties=int(i[7]),
-                Approach_accidents=int(i[8]),
-                Approach_fatalities=int(i[9]),
-                Landing_accidents=int(i[10]),
-                Landing_fatalities=int(i[11])
-            )
-            sess.add(new)
-        sess.commit()
+    with open('Aviation accidents and fatalities by flight phase (ASN, 2019).csv', encoding='utf-8', mode='r') as file:
+        data = csv.reader(file, delimiter=',')
+        data.__next__()
 
-Base.metadata.create_all(engine)
+        with Session(engine) as sess:
+            for i in data:
+                new = AviationAccidents(
+                    Entity=i[0],
+                    Year=int(i[1]),
+                    En_route_accidents=int(i[2]),
+                    En_route_casualties=int(i[3]),
+                    Take_off_accidents=int(i[4]),
+                    Take_off_casualties=int(i[5]),
+                    Initial_climb_accidents=int(i[6]),
+                    Initial_climb_casualties=int(i[7]),
+                    Approach_accidents=int(i[8]),
+                    Approach_fatalities=int(i[9]),
+                    Landing_accidents=int(i[10]),
+                    Landing_fatalities=int(i[11])
+                )
+                sess.add(new)
+            sess.commit()
 
-#запрос выдаёт количество смертей при посадке (2 число) за год (1 чилсо)
-with Session(engine) as sess2:
-    q_1 = sess2.execute(
-        select(AviationAccidents.Year, AviationAccidents.Landing_fatalities)
-        .where(AviationAccidents.Entity == 'World') # -_-
-        .order_by(AviationAccidents.Year.desc()))
-    print(q_1.all())
+    Base.metadata.create_all(engine)
 
+    #запрос выдаёт количество смертей при посадке (2 число) за год (1 чилсо)
+    with Session(engine) as sess2:
+        q_1 = sess2.execute(
+            select(AviationAccidents.Year, AviationAccidents.Landing_fatalities)
+            .where(AviationAccidents.Entity == 'World') # -_-
+            .order_by(AviationAccidents.Year.desc()))
+        print(q_1.all())
+
+else:
+    print("Вы уже всё посмотрели")
