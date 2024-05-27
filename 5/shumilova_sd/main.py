c9bdd4de4e3487e9ce8dd33eafb3a396dc8cbcaf
sonmur <149419693+sonmur@users.noreply.github.com>
2024-05-28 00:19:42 +0300

    Update main.py

--- 5/shumilova_sd/main.py
+++ 5/shumilova_sd/main.py
@@ -4,11 +4,12 @@ from models import engine, Deaths
 
 Deaths.metadata.create_all(engine)
 
-with Session(engine) as s:
-    with open('Malaria deaths - IHME (2016).csv', newline='') as f:
-        reader = csv.reader(f)
-        next(reader)
+with open('Malaria deaths - IHME (2016).csv', newline='') as f:
+    reader = csv.reader(f)
+    reader.__next__()
+
+    with Session(engine) as s:
         for r in reader:
-                data = Deaths(entity=r[0], year=int(r[1]), deaths=float(r[2]))
-                s.add(data)
-                s.commit()
+            data = Deaths(entity=r[0], year=int(r[1]), deaths=float(r[2]))
+            s.add(data)
+        s.commit()
