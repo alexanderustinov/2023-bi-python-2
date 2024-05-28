4926254fab913518a8e94ea7dac413a29a1abc26
sonmur <149419693+sonmur@users.noreply.github.com>
2024-05-28 13:02:59 +0300

    Update hashing.py

--- 7/shumilova_sd/hashing.py
+++ 7/shumilova_sd/hashing.py
@@ -1,5 +1,6 @@
 from cracking import hash
 
+print('Пароль должен содержать не более 4-х символов. Может состоять из латинских букв, цифр и/или специальных символов.')
 password = input('Please, enter your password: ')
 
 with open('password.txt', 'w') as file:
