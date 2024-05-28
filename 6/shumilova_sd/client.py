42f2469e32a18e64f0069bbe140b0744df499fbb
sonmur <149419693+sonmur@users.noreply.github.com>
2024-05-28 17:12:13 +0300

    Update client.py

--- 6/shumilova_sd/client.py
+++ 6/shumilova_sd/client.py
@@ -1,27 +1,26 @@
 import requests
+from random import randrange
+from schemas import StarsTest
+
 
 s = requests.Session()
 api_base = 'http://127.0.0.1:8000'
 
+
 def main():
-    stars = [{"name": "Sun", "distance": "149.6*(10**6)"},
-             {"name": "Alpha Centauri", "distance": "41.3*(10**12)"},
-             {"name": "Proxima Centauri", "distance": "39.9*(10**12)"},
-             {"name": "Sirius", "distance": "81*(10**12)"}]
+    names = ["Sun", "Alpha Centauri", "Proxima Centauri", "Sirius"]
+    stars = []
+    for i in names:
+        cls = StarsTest(name=i, radius=randrange(1000, 5000), mass=randrange(5000, 100000),
+                        distance=randrange(10000, 1000000))
+        stars.append(cls)
+    for i in stars:
+        s.put(f"{api_base}/add", i.model_dump_json())
 
-    for _ in stars:
-        a = s.put(f"{api_base}/add", json=_)
-        print(a.text)
+    for i in names:
+        res = s.get(f"{api_base}/stars/{i}/dis")
+        print(f"Your star is {i}, and it's distance from the Earth is {res.json()}")
 
-    #r = s.get(api_base)
-
-    star_name = "Sun"
-    res = requests.get(f"{api_base}/{star_name}")
-    print(f"Your star is {star_name}, and it's distance from the Earth is {res.text}")
-
-    star_name_ = "Sirius"
-    res_ = requests.get(f"{api_base}/{star_name_}/dis")
-    print(f"Your star is {star_name_}, and it's distance from the Earth is {res_.text}")
 
 if __name__ == '__main__':
     main()
